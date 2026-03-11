e0f12e4c2a70c3a5fa568a34d68b95b9d6f326a1
Holly Tomren <htomren@gmail.com>
2026-03-11 13:57:26 -0400

    Update asrs_batch_request.py

--- asrs/asrs_batch_request.py
+++ asrs/asrs_batch_request.py
@@ -8,7 +8,7 @@
 ## The file needs four columns with these headers: mms_id, holding_id, pid, description
 ## Each row will contain the data for the items you would like to request. All columns are required.
 ## You can obtain the MMS ID, Holdings ID, Physical Item ID, and Physical Item Description for each item using Alma Analytics.
-## The file should contain request data for no more than 1000 items, due to Alma API limitations.
+## The file should contain request data for no more than 1000 items, due to daily Alma API limitations.
 ## The file should be in the same directory as this script (if not, change the path in the code below).
 ##
 ## 3. Set the Alma user ID of the requester below
